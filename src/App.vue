ac4751c835060e81018d6b37663879bf1962ed91
Thira <tintha@gmail.com>
2021-09-26 00:02:01 -0400

    form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
+      <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          temporary
+      >
+        <v-list>
+          <v-list-item v-for="link in links" :key="link">
+            <v-list-item-action>
+              <v-list-item-icon>x</v-list-item-icon>
+            </v-list-item-action>
+            <v-list-item-title>
+              {{ link }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <hello-world />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
     HelloWorld
-  }
-}
-</script>
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+    drawer: false,
+    links: ["Dashboard", "About", "Contact"],
+  }),
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center" justify="center">
+      <v-col class="mb-5" cols="12">
+        <h1>{{ title }}</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-form>
+          <v-text-field label="Title" v-model="form.cardTitle"></v-text-field>
+          <v-text-field
+            label="Sub-Title"
+            v-model="form.cartSubTitle"
+          ></v-text-field>
+          <v-text-field label="Text" v-model="form.cardText"></v-text-field>
+          <v-btn type="submit" class="primary" @click.prevent="submitForm(form)"
+            >Submit</v-btn
+          >
+        </v-form>
+      </v-col>
+      <v-col>
+        <p v-if="error">{{ error | caps }}</p>
+        <transition name="fade">
+          <v-card v-if="submitted">
+            <v-card-title>{{ form.cardTitle }}</v-card-title>
+            <v-card-subtitle>{{ form.cartSubTitle }}</v-card-subtitle>
+            <v-card-text>
+              {{ form.cardText }}
+            </v-card-text>
+            <v-card-actions
+              ><v-icon @click.prevent="clearCard">mdi-close</v-icon
+              ><v-icon>mdi-heart</v-icon></v-card-actions
+            >
+          </v-card>
+        </transition>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data: () => ({
+    title: "Hello world!",
+    error: "",
+    submitted: false,
+    form: {
+      cardText: "",
+      cardTitle: "",
+      cartSubTitle: "",
+    },
+  }),
+  methods: {
+    submitForm(data) {
+      console.log(data);
+      if (
+        !this.form.cardText ||
+        !this.form.cardTitle ||
+        !this.form.cartSubTitle
+      ) {
+        this.error = "All fields must be completed.";
+      } else {
+        this.error = null;
+        this.submitted = true;
+      }
+    },
+    clearCard() {
+      this.form = {
+        cardText: "",
+        cardTitle: "",
+        cartSubTitle: "",
+      };
+      this.submitted = false;
+    },
+  },
+  filters: {
+    caps: function(value) {
+      return value.toUpperCase();
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.fade-enter {
+  opacity: 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.fade-enter-active {
+  transition: all 1s ease-in;
 }
 </style>
